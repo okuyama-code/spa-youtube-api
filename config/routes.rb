51f5b1cab839b48b267fe3a744526293d0602c1f
okuyama-code <130431374+okuyama-code@users.noreply.github.comm>
2023-12-24 12:54:19 +0900

    change routes to use /api/v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :posts
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # api/v1
+  namespace :api do
+    namespace :v1 do
+      resources :posts
+    end
+  end
 end
