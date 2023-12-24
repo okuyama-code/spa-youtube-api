fd7da3bdd095e73493daca8f6294f8e9eb852785
okuyama-code <130431374+okuyama-code@users.noreply.github.comm>
2023-12-24 12:39:16 +0900

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+Post.destroy_all
+
+# =========== create =================
+
+20.times do
+  Post.create(
+    title: Faker::Lorem.sentence(word_count: 3),
+    body: Faker::Lorem.paragraph(sentence_count: 3)
+  )
+end
